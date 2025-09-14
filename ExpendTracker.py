b19d07522727a777a863baa19e9c8fa55f270340
selma pasavide <slmpasavideh@gmail.com>
2025-09-14 15:07:26 +0330

    feat: add Expense class with basic attributes

---
+++ ExpendTracker.py
@@ -0,0 +1,6 @@
+class Expend():
+    def __init__(self, date, category, description, amount):
+        self.date = date
+        self.category = category
+        self.description = description
+        self.amount = amount
