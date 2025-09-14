4ef22ff025ed68aabc802b19a64be7458e54097d
selma pasavide <slmpasavideh@gmail.com>
2025-09-14 17:44:03 +0330

    fix: format amounts with thousand separators for better readabiliy

--- ExpendTracker.py
+++ ExpendTracker.py
@@ -26,11 +26,11 @@ class Expense_Tracker():
         else:
             print("*** Expense List ***")
             for i, expense in enumerate(self.expenses, start=1):
-                print(f"{i}. Date: {expense.date}, Category: {expense.category}, Description: {expense.description}, Amount: {expense.amount:.3f}T")
+                print(f"{i}. Date: {expense.date}, Category: {expense.category}, Description: {expense.description}, Amount: {expense.amount:,}T")
 
     def total_expenses(self):
         total = sum(expense.amount for expense in self.expenses)
-        print(f"Total Expenses: {total:.3f}T")
+        print(f"Total Expenses: {total:,}T")
 
 
 def main():
